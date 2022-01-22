36e7510fafda9c212c543b6cb6d0adf1eaf80db2
CarlosZoft <carlosoftwar@gmail.com>
2022-01-22 14:27:55 -0300

    🐛 fix: change label to children
    Co-authored-by: Jônatas A. <jonatasaraujo2257@gmail.com>

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -20,22 +20,22 @@ export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
     primary: true,
-    label: 'Button',
+    children: 'Button',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-    label: 'Button',
+    children: 'Button',
 };
 
 export const Large = Template.bind({});
 Large.args = {
     size: 'large',
-    label: 'Button',
+    children: 'Button',
 };
 
 export const Small = Template.bind({});
 Small.args = {
     size: 'small',
-    label: 'Button',
+    children: 'Button',
 };
