b35a9aa1d2d6fbcff7721e5f61970d9b2238ee57
CarlosZoft <carlosoftwar@gmail.com>
2022-01-22 14:29:16 -0300

    ✨ feat : created button group component
    Co-authored-by: Jônatas A. <jonatasaraujo2257@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Button } from './components/button';
 export { Input } from './components/input';
 export { Checkbox } from './components/checkbox';
+export { ButtonGroup } from './components/buttonGroup';
---
+++ src/stories/buttonGroup.stories.tsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ButtonGroup, Button } from '../';
+
+const config = {
+    buttonList: [
+        {
+            label: 'Purffect',
+            color: '#e9c46a',
+            backgroundColor: '#264653',
+        },
+        {
+            label: 'Xurumin',
+            color: '#e9c46a',
+            backgroundColor: '#264653',
+        },
+        {
+            label: 'Clean Software',
+            color: '#e9c46a',
+            backgroundColor: '#264653',
+        },
+    ],
+    props: {
+        style: { width: '350px' },
+    },
+};
+
+export default {
+    title: 'Example/ButtonGroup',
+    component: ButtonGroup,
+
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof ButtonGroup>;
+
+const Template: ComponentStory<typeof ButtonGroup> = (args) => <ButtonGroup {...args} />;
+
+export const Row = Template.bind({});
+Row.args = {
+    children: (
+        <>
+            {config.buttonList.map((button, index) => (
+                <button
+                    key={`${index.toString()} key`}
+                    style={{
+                        color: button.color,
+                        backgroundColor: button.backgroundColor,
+                    }}
+                >
+                    {button.label}
+                </button>
+            ))}
+        </>
+    ),
+    direction: 'row',
+    ...config.props,
+};
+export const Column = Template.bind({});
+Column.args = {
+    children: (
+        <>
+            {config.buttonList.map((button, index) => (
+                <button
+                    key={`${index.toString()} key`}
+                    style={{
+                        color: button.color,
+                        backgroundColor: button.backgroundColor,
+                    }}
+                >
+                    {button.label}
+                </button>
+            ))}
+        </>
+    ),
+    ...config.props,
+    direction: 'column',
+};
+export const WithLibButton = Template.bind({});
+WithLibButton.args = {
+    children: (
+        <>
+            {config.buttonList.map((button, index) => (
+                <Button
+                    key={`${index.toString()} key`}
+                    style={{
+                        color: button.color,
+                        backgroundColor: button.backgroundColor,
+                    }}
+                >
+                    {button.label}
+                </Button>
+            ))}
+        </>
+    ),
+    ...config.props,
+    direction: 'column',
+};
